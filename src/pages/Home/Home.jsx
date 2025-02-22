857fa77a1f27669fac398551287a4111cdc48c12
nadimmo <nadimmostafa334@gmail.com>
2025-02-22 16:36:42 +0600

    added category and author section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Hero from '../../components/Hero/Hero'
 import LatestPost from '../../components/LatestPost/LatestPost'
 import FeaturedBlogs from '../../components/FeaturedBlogs/FeaturedBlogs'
+import Categories from '../../components/Categories/Categories'
+import Author from '../../components/Author/Author'
 
 const Home = () => {
   return (
@@ -9,6 +11,8 @@ const Home = () => {
       <Hero />
       <LatestPost />
       <FeaturedBlogs/>
+      <Categories/>
+      <Author/>
     </div>
   )
 }
